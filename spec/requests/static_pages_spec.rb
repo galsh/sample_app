bffd2eb0f62331f59f78b56856221aad7ccafa4a
Galsh <gav.walsh@gmail.com>
2013-08-10 21:57:11 +0100

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,10 @@ describe "Static Pages" do
 		it "should have the content 'Sample App'" do
 
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text => 'Sample App')
+
+			page.should have_selector('title', 
+				:text => "Ruby on Rails Tutorial Sample App | Home")
 		end
 	end
 
@@ -16,7 +19,9 @@ describe "Static Pages" do
 		it "should have the content help" do
 
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text => 'Help')
+
+			
 		end
 	end
 
@@ -34,7 +39,7 @@ describe "Static Pages" do
 		it "should have the content 'Contact Us'" do
 
 			visit '/static_pages/contact'
-			page.should have_content('Contact Us')
+			page.should have_selector('h1', :text => 'Contact Us')
 		end
 	end
 
