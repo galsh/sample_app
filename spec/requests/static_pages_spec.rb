798690bf7286a39974367940940f122769477040
Galsh <gav.walsh@gmail.com>
2013-08-10 21:20:12 +0100

    change rspec so it works with Sublime

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+	describe "Home Page" do
+
+		it "should have the content 'Sample App'" do
+
+			visit '/static_pages/home'
+			page.should have_content('Sample App')
+		end
+	end
+
+	describe "Help Page" do
+
+		it "should have the content help" do
+
+			visit '/static_pages/help'
+			page.should have_content('Help')
+		end
+	end
+
+	describe "About Page" do
+
+		it "should have the content 'About Us'" do
+
+			visit '/static_pages/about'
+			page.should have_content('About Us')
+		end
+	end
+
+	describe "Contact Us Page" do
+
+		it "should have the content 'Contact Us'" do
+
+			visit '/static_pages/contact'
+			page.should have_content('Contact Us')
+		end
+	end
+
+
+
+end
