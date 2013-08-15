2df0f1c3951d6584ebd95e89bd31bf3a8c67833e
Galsh <gav.walsh@gmail.com>
2013-08-15 14:06:33 +0100

    add sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
-  def new
-  end
+  
   def show
   	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	#load up the new page
+  	@user = User.new
+  	render 'new'
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,4 +28,26 @@ describe "UserPages" do
 		it { should have_selector('h1',	text: user.name) }
 		it { should have_selector('title', text: user.name) }
 	end
+
+	describe "signup" do
+		before { visit signup_path }
+		
+
+		describe "with invalid information" do
+			it "should  not create a user" do
+				old_count = User.count
+				click_button "Create my account"
+				new_count = User.count
+				new_count.should == old_count
+
+
+
+			end
+		end
+
+		describe  "with valid information" do
+			it "should create a user" do
+			end
+		end
+	end
 end
