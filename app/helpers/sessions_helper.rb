f788c0d41b068313c3d0291e50b8adc0ced61f60
Galsh <gav.walsh@gmail.com>
2013-08-18 12:21:32 +0100

    add index to search users email

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,6 @@ module SessionsHelper
 # by default these are available in the views and not the controller
 # need to include SessionsHelper in the application conroller
 	def sign_in(user)
-		cookies[:remember_Token] = { value: user.remember_Token, expires: 20.years.from_now }
+		cookies[:remember_token] = { value: user.remember_token, expires: 20.years.from_now }
 	end
 end
