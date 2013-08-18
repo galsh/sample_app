e217d0f727f8d3e66ebcfbe25113afa8bfdefed1
Galsh <gav.walsh@gmail.com>
2013-08-18 11:02:32 +0100

    migrate remember token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,6 @@ module SessionsHelper
 # by default these are available in the views and not the controller
 # need to include SessionsHelper in the application conroller
 	def sign_in(user)
-		
+		cookies[:remember_Token] = { value: user.remember_Token, expires: 20.years.from_now }
 	end
 end
